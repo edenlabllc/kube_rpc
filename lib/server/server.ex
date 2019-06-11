51e6b2c296864809ffff5dd8778ca536f5b5f168
Alex Kovalevych <alexkovalevych@gmail.com>
2019-06-11 16:13:45 +0300

    feat: add health check support

--- lib/server/server.ex
+++ lib/server/server.ex
@@ -40,6 +40,18 @@ defmodule KubeRPC.Server do
   end
 
   @impl true
+  def handle_cast(:check, state) do
+    Task.start(fn ->
+      case File.open(System.get_env("HEALTH_CHECK_PATH") || "/tmp/healthy", [:write]) do
+        {:ok, pid} -> File.close(pid)
+        error -> Logger.error("Failed to create file: #{inspect(error)}")
+      end
+    end)
+
+    {:noreply, state}
+  end
+
+  @impl true
   def handle_info(_, state) do
     {:noreply, state}
   end
